cb4c9eff31996607d973daf54d8c6df2992e81a9
Agostina Pedano <pedanoagostina@gmail.com>
2023-09-26 19:57:15 -0300

    second commit

--- newJava-app/jenkinsTrial.java
+++ newJava-app/jenkinsTrial.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("A commit has been made.");
 	System.out.println("Hello");
+	System.out.println("hey hey");
     }
 }
